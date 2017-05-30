afc0d97f81895309e980b1679543d1cb04d9c32d
Jacek Piłka <jacek001@wp.pl>
2017-05-30 17:21:10 +0200

    Jeszcze czyściej.

--- src/gawarietGawariet/server/Server.java
+++ src/gawarietGawariet/server/Server.java
@@ -42,21 +42,7 @@ public class Server {
             System.out.println(message);
             response=setStatus(message, response, address, port);
 
-
-             /* A tu to samo co wyzej w jednej linijce
-             * przyklad potegi programowania obiektowego
-             */
-
             //currentStatus = Status.getStatus(message);
-            
-            /**
-             * Co to za potęga, skoro nie działa tak jak powinna?
-             * Lepsze chłopskie rozwiązanie, które przynajmniej działa dobrze.
-             */
-
-            /**
-             * O nie to jest wszsytko w static mainie ....aaaa
-             */
             datagramSocket.send(response);
         }
     }
@@ -103,87 +89,107 @@ public class Server {
     
     static DatagramPacket checkStatus(String message, DatagramPacket response, InetAddress address, int port) throws Exception{
     	if (currentStatus == status.login) {    //Logowanie użytkownika
-            if (mesgCounter == 0) {    //Pierwsza informacja - LOGIN
-                tmpLogin = message;
-                mesgCounter++;
-            } else if (mesgCounter == 1) {    //Druga informacja - hasło
-                tmpPassword = message;
-                mesgCounter = 0;
-                boolean userExists = false;
-                for (int i = 0; i < users.size(); i++) {    //Szukaj użytkownika
-                    if (users.get(i).login.equals(tmpLogin) & users.get(i).password.equals(tmpPassword)) {    //Istnieje
-                        users.get(i).online = true;
-                        users.get(i).lastAddress = address;
-                        users.get(i).lastPort = port;
-                        userExists = true;
-                        response = new DatagramPacket(
-                                "Logged".getBytes("utf8"), "Logged".getBytes("utf8").length, address, port);
-                        break;
-                    } else if (users.get(i).login.equals(tmpLogin)) {    //Złe hasło
-                        userExists = true;
-                        response = new DatagramPacket(
-                                "WrongLog".getBytes("utf8"), "WrongLog".getBytes("utf8").length, address, port);
-                        break;
-                    }
-                }
-                if (!userExists) {    //Rejestracja i logowanie
-                    users.add(new User(tmpLogin, tmpPassword));
-                    users.get(users.size() - 1).online = true;
-                    users.get(users.size() - 1).lastAddress = address;
-                    users.get(users.size() - 1).lastPort = port;
-                    response = new DatagramPacket(
-                            "Registered".getBytes("utf8"), "Registered".getBytes("utf8").length, address, port);
-                }
-                currentStatus = status.idle;
-            }
+    		response=login(message, response, address, port);
         } else if (currentStatus == status.logout) {    //Wyloguj użytkownika, z którego adresu przyszła informacja
-            for (int i = 0; i < users.size(); i++) {
-                if (users.get(i).lastAddress.equals(address)) {
-                    users.get(i).online = false;
-                    users.get(i).busy = false;
-                    users.get(i).currentPal = null;
-                }
-            }
+        	response=logout(message, response, address, port);
             currentStatus = status.idle;
         } else if (currentStatus == status.palSelect) {    //Wybór adresata
-            for (int i = 0; i < users.size(); i++) {    //Szukaj adresata
-                if (users.get(i).login.equals(message)) {
-                    for (int j = 0; j < users.size(); i++) {    //Szukaj wysyłającego
-                        if (users.get(j).lastAddress.equals(address)) {
-                            if (users.get(i).busy) {    //Jeżeli użytkownik zajęty
-                                response = new DatagramPacket(
-                                        "BusyPal".getBytes("utf8"), "BusyPal".getBytes("utf8").length, address, port);
-                            } else {
-                                users.get(j).currentPal = users.get(i);
-                                users.get(i).currentPal = users.get(j);
-                                users.get(j).busy = true;
-                                users.get(i).busy = true;
-                                response = new DatagramPacket(
-                                        "Connected".getBytes("utf8"), "Connected".getBytes("utf8").length, address, port);
-                            }
-                            break;
-                        }
-                        response = new DatagramPacket(
-                                "NoPal".getBytes("utf8"), "NoPal".getBytes("utf8").length, address, port);
-                    }
+        	response=palSelect(message, response, address, port);
+            currentStatus = status.idle;
+        } else if (currentStatus == status.send) {    //Wyślij wiadomość od użytkownika
+        	response=send(message, response, address, port);
+        	currentStatus = status.idle;
+        }
+    	return response;
+    }
+    
+    static DatagramPacket login(String message, DatagramPacket response, InetAddress address, int port) throws Exception{
+    	if (mesgCounter == 0) {    //Pierwsza informacja - LOGIN
+            tmpLogin = message;
+            mesgCounter++;
+        } else if (mesgCounter == 1) {    //Druga informacja - hasło
+            tmpPassword = message;
+            mesgCounter = 0;
+            boolean userExists = false;
+            for (int i = 0; i < users.size(); i++) {    //Szukaj użytkownika
+                if (users.get(i).login.equals(tmpLogin) & users.get(i).password.equals(tmpPassword)) {    //Istnieje
+                    users.get(i).online = true;
+                    users.get(i).lastAddress = address;
+                    users.get(i).lastPort = port;
+                    userExists = true;
+                    response = new DatagramPacket(
+                            "Logged".getBytes("utf8"), "Logged".getBytes("utf8").length, address, port);
+                    break;
+                } else if (users.get(i).login.equals(tmpLogin)) {    //Złe hasło
+                    userExists = true;
+                    response = new DatagramPacket(
+                            "WrongLog".getBytes("utf8"), "WrongLog".getBytes("utf8").length, address, port);
                     break;
                 }
             }
-            currentStatus = status.idle;
-        } else if (currentStatus == status.send) {    //Wyślij wiadomość od użytkownika
-            for (int i = 0; i < users.size(); i++) {
-                if (users.get(i).lastAddress.equals(address)) {
-                    message = users.get(i).login + ": " + message + "\n";
-                    users.get(i).currentPal.unreadMesg.add(message);
-                    users.get(i).currentPal.unreadMesg.get(0);//Nie wiadomo czemu, ale potrzebne do działania
-                    response = new DatagramPacket(
-                            message.getBytes("utf8"),
-                            message.getBytes("utf8").length, address, port);
-                    break;
-                }
+            if (!userExists) {    //Rejestracja i logowanie
+                users.add(new User(tmpLogin, tmpPassword));
+                users.get(users.size() - 1).online = true;
+                users.get(users.size() - 1).lastAddress = address;
+                users.get(users.size() - 1).lastPort = port;
+                response = new DatagramPacket(
+                        "Registered".getBytes("utf8"), "Registered".getBytes("utf8").length, address, port);
             }
             currentStatus = status.idle;
         }
-    	return response;
+        return response;
+    }
+    
+    static DatagramPacket logout(String message, DatagramPacket response, InetAddress address, int port) throws Exception{
+    	for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).lastAddress.equals(address)) {
+                users.get(i).online = false;
+                users.get(i).busy = false;
+                users.get(i).currentPal = null;
+            }
+        }
+        return response;
+    }
+    
+    static DatagramPacket palSelect(String message, DatagramPacket response, InetAddress address, int port) throws Exception{
+    	for (int i = 0; i < users.size(); i++) {    //Szukaj adresata
+            if (users.get(i).login.equals(message)) {
+                for (int j = 0; j < users.size(); i++) {    //Szukaj wysyłającego
+                    if (users.get(j).lastAddress.equals(address)) {
+                        if (users.get(i).busy) {    //Jeżeli użytkownik zajęty
+                            response = new DatagramPacket(
+                                    "BusyPal".getBytes("utf8"), "BusyPal".getBytes("utf8").length, address, port);
+                        } else {
+                            users.get(j).currentPal = users.get(i);
+                            users.get(i).currentPal = users.get(j);
+                            users.get(j).busy = true;
+                            users.get(i).busy = true;
+                            response = new DatagramPacket(
+                                    "Connected".getBytes("utf8"), "Connected".getBytes("utf8").length, address, port);
+                        }
+                        break;
+                    }
+                    response = new DatagramPacket(
+                            "NoPal".getBytes("utf8"), "NoPal".getBytes("utf8").length, address, port);
+                }
+                break;
+            }
+    	}
+        return response;
+    }
+    
+    static DatagramPacket send(String message, DatagramPacket response, InetAddress address, int port) throws Exception{
+    	for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).lastAddress.equals(address)) {
+                message = users.get(i).login + ": " + message + "\n";
+                users.get(i).currentPal.unreadMesg.add(message);
+                users.get(i).currentPal.unreadMesg.get(0);//Nie wiadomo czemu, ale potrzebne do działania
+                response = new DatagramPacket(
+                        message.getBytes("utf8"),
+                        message.getBytes("utf8").length, address, port);
+                break;
+            }
+        }
+        return response;
     }
 }
